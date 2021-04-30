6eaba39b6ed177ded15d056016b0bf412edf25f0
joshua ayomide <4surejosh@gmail.com>
2021-05-01 01:53:51 +0200

    node version specified

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
-const baseUrl = location.hostname === "localhost" ? "http://localhost:2000" : "https://josebekadmin.herokuapp.com";
+const baseUrl = 'https://josebekadmin.herokuapp.com';
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (filename) => {
-    return `${baseUrl}/${filename}`;
-}
+	return `${baseUrl}/${filename}`;
+};
